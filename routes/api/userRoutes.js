169de88440346c9bbba65e716979c0e6090f9a76
bechedley <109792989+bechedley@users.noreply.github.com>
2023-01-04 14:55:43 +1100

    Add thought routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -10,12 +10,21 @@ const {
 } = require('../../controllers/userController');
 
 // /api/users
-router.route('/').get(getUsers).post(createUser);
+router.route('/')
+.get(getUsers)
+.post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route('/:userId')
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser);
+
+// /api/users/:userId/friends
+router.route('/:userId/friends')
+.post(addFriend);
 
 // /api/users/:userId/friends/:friendId
-router.route('/:userId/friends/:friendId').put(addFriend).delete(removeFriend);
+router.route('/:userId/friends/:friendId').delete(removeFriend);
 
 module.exports = router;
